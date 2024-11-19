dfa6524d3eeb115859fbc02d185f644f8435f03e
Yunusov Roman <166059736+YunusovRA@users.noreply.github.com>
2024-11-19 16:35:27 +0300

    add implemented

--- src/main/java/ru/netology/PhoneBook.java
+++ src/main/java/ru/netology/PhoneBook.java
@@ -1,7 +1,13 @@
 package ru.netology;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
+    private Map<String, String> contacts = new HashMap<>();
+
     public int add(String name, String number) {
-        return 0;
+        contacts.putIfAbsent(name, number);
+        return contacts.size();
     }
 }
