79b78de9e8677db0ed788c793c30895baf3a737f
Yunusov Roman <166059736+YunusovRA@users.noreply.github.com>
2024-11-19 16:27:59 +0300

    Stub and tests for add

--- src/test/java/ru/netology/PhoneBookTest.java
+++ src/test/java/ru/netology/PhoneBookTest.java
@@ -1,5 +1,14 @@
 package ru.netology;
 
-public class PhoneBookTest {
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
+public class PhoneBookTest {
+    @Test
+    public void testAdd() {
+        PhoneBook phoneBook = new PhoneBook();
+        assertEquals(1, phoneBook.add("Alice", "12345"));
+        assertEquals(2, phoneBook.add("Bob", "67890"));
+        assertEquals(2, phoneBook.add("Alice", "12345"));
+    }
 }
